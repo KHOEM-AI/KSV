c86ac631f3519d81b2ff73d19335754804859b6b
KHOEM_AI <khoemaikhoemai@gmail.com>
2026-08-31 23:18:47 +0700

    ID_AI_369_400_401

---
+++ khoem-now/src/components/KSVSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function KSVSection() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>KSV</h1>
+
+      {/* ដាក់ Interface និង UI របស់ KSV នៅទីនេះ */}
+      <p>ផ្នែក KSV នឹងបង្ហាញនៅទីនេះ។</p>
+    </main>
+  );
+}
