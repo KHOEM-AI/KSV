4bea188f0dd71aed6ef00f4cf012175c10e61de4
KHOEM_AI <khoemaikhoemai@gmail.com>
2026-09-19 03:31:59 +0700

    ID_AI_369_400_401

---
+++ khoem-now/src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+// import identityRoutes from '../modules/identity/routes';
+
+const router = Router();
+
+// router.use('/identity', identityRoutes);
+// router.use('/device', deviceRoutes);
+// router.use('/organization', organizationRoutes);
+
+export default router;
