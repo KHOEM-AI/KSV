91a4081ce23a3d3268de31d7d0489be193243cab
khoem401grab-lgtm <vutha2019@gmail.com>
2026-09-18 18:07:01 +0700

    refactor: organize scripts and archive files, remove unused server routes

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'khoem-now/**', 'archive/**', '*.cjs', 'KSV-server-routes.js'] },
+  { ignores: ['dist', 'khoem-now/**', 'archive/**', '*.cjs'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
