f3bc02cc71c7c6e16843439f2f84d74b3429d9a7
KHOEM_AI <khoemaikhoemai@gmail.com>
2026-09-19 07:38:48 +0700

    ID_AI_369_400_401

--- khoem-now/src/routes/index.ts
+++ khoem-now/src/routes/index.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
-// import identityRoutes from '../modules/identity/routes';
+import { identityRoutes } from '../modules/identity';
+import { organizationRoutes } from '../modules/organization';
+import { deviceRoutes } from '../modules/device';
+import { discoveryRoutes } from '../modules/discovery';
+import { commandRoutes } from '../modules/command';
+import { gatewayRoutes } from '../modules/communication/gateway';
+import { protocolRoutes } from '../modules/communication/protocol';
+import { safetyRoutes } from '../modules/safety';
 
 const router = Router();
 
-// router.use('/identity', identityRoutes);
-// router.use('/device', deviceRoutes);
-// router.use('/organization', organizationRoutes);
+router.use('/identity', identityRoutes);
+router.use('/organization', organizationRoutes);
+router.use('/device', deviceRoutes);
+router.use('/discovery', discoveryRoutes);
+router.use('/command', commandRoutes);
+router.use('/gateway', gatewayRoutes);
+router.use('/protocol', protocolRoutes);
+router.use('/safety', safetyRoutes);
 
 export default router;
